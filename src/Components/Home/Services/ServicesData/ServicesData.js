f35dfa45999e4f4a1c4a005ec4f8194254d8a84b
JH-Jitu <jitu000166@gmail.com>
2020-10-13 20:27:08 +0600

    up2

--- src/Components/Home/Services/ServicesData/ServicesData.js
+++ src/Components/Home/Services/ServicesData/ServicesData.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ServicesData.css'
 
 const ServicesData = ({ service }) => {
     return (
         <div className="col-md-4">
             <div className="m-1 text-center">
-                <img style={{ height: '50px' }} src={service.img} alt="" />
+                <img className="serviceImg" src={service.img} alt="" />
                 <h5 className="mt-3 mb-2"><strong>{service.name}</strong> </h5>
                 <p className="text-secondary">{service.desc}</p>
             </div>
